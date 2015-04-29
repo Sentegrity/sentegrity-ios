8018979e40f139e3588151f30c36582aa2d2a2f2
Jason Sinchak <jsinchak@sentegrity.com>
2015-04-29 18:48:15 -0500

    test

--- SenTest/SenTest/ViewController.m
+++ SenTest/SenTest/ViewController.m
@@ -3,7 +3,7 @@
 //  SenTest
 //
 //  Created by Walid Javed on 1/16/15.
-//  Copyright (c) 2015 Walid Javed. All rights reserved.
+//  Copyright (c) 2015 Walid Javed. All rights reserved. test
 //  
 
 #import "ViewController.h"
